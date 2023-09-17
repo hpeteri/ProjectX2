49ac3111e6f921481bb0237ba06e836196fd19b3
Henrik Peteri <p.henrik.rikhard@hotmail.com>
2023-09-17 11:49:02 +0300

    Add tests

--- main.cpp
+++ main.cpp
@@ -1,8 +1,89 @@
 #include "src/calculator.h"
+#include "src/calculator.h"
+
+#include <assert.h>
+#include <limits.h>
+
+void ut_add(void)
+{
+    int result;
+
+    result = calculator(1, '+', 1);
+    assert(result == 2);
+
+    result = calculator(1, '+', -1);
+    assert(result == 0);
+
+    result = calculator(-1, '+', -1);
+    assert(result == -2);
+
+    result = calculator(INT_MIN, '+', -1);
+    assert(result == INT_MAX);
+
+    result = calculator(INT_MAX, '+', 1);
+    assert(result == INT_MIN);
+}
+
+void ut_subtract(void)
+{
+    int result;
+
+    result = calculator(1, '-', 1);
+    assert(result == 0);
+
+    result = calculator(1, '-', -1);
+    assert(result == 2);
+
+    result = calculator(-1, '-', -1);
+    assert(result == 0);
+
+    result = calculator(INT_MIN, '-', 1);
+    assert(result == INT_MAX);
+
+    result = calculator(INT_MAX, '-', -1);
+    assert(result == INT_MIN);
+}
+
+void ut_multiply(void)
+{
+    int result;
+
+    result = calculator(20, '*', -1);
+    assert(result == -20);
+
+    result = calculator(20, '*', 0);
+    assert(result == 0);
+
+    result = calculator(0, '*', 20);
+    assert(result == 0);
+
+    result = calculator(INT_MAX, '*', -1);
+    assert(result == INT_MIN + 1);
+
+    result = calculator(INT_MIN, '*', -1);
+    assert(result == INT_MAX + 1); //overflow
+}
+
+void ut_divide(void)
+{
+    int result;
+
+    result = calculator(20, '/', -1);
+    assert(result == -20);
+
+    result = calculator(20, '/', 0);
+    assert(result == 1);
+
+    result = calculator(0, '/', 20);
+    assert(result == 0);
+}
 
 int main() {
-    // Function calls to calculate
-    // Write your all possible input scenarios here
-    // Notice that calculator code written here works with floats now
+
+    ut_add();
+    ut_subtract();
+    ut_multiply();
+    ut_divide();
+
     return 0;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -6,10 +6,20 @@
 #include <cstddef>
 #include <string>
 
-FUZZ_TEST_SETUP() {
-  // Perform any one-time setup required by the FUZZ_TEST function.
+FUZZ_TEST_SETUP()
+{
+    // Perform any one-time setup required by the FUZZ_TEST function.
 }
 
-FUZZ_TEST(const uint8_t *data, size_t size) {
-  /*Your code here*/
+FUZZ_TEST(
+    const uint8_t *data,
+    size_t size)
+{
+    FuzzedDataProvider fuzzed_data(data, size);
+
+    float a  = fuzzed_data.ConsumeIntegral<int>();
+    float b  = fuzzed_data.ConsumeIntegral<int>();
+    char  op = fuzzed_data.ConsumeIntegral<char>();
+
+    float result = calculator(a, op, b);
 }
